7865aa012c122c41f7d47eb0565a9a18fb0ea173
Louise Lytken Petersen Møller <loui142j@edu.easj.dk>
2019-02-01 13:31:04 +0100

    Added FlatWhite class

---
+++ KaffeConsoleAppNetCore/FlatWhite.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using kaffe;
+
+namespace KaffeConsoleAppNetCore
+{
+    class FlatWhite : Kaffe, Imælk
+    {
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+        public override int Pris()
+        {
+            return 45;
+        }
+
+        public override string Styrke()
+        {
+            return "Mild";
+        }
+    }
+}
